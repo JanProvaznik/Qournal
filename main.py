93b063493c3bc46ee08fe6ef643ae90723a55bfa
Jan Provaznik <janproxdd@gmail.com>
2020-12-23 00:53:52 +0100

    More dictionaries and serialization through dataclass and jsons.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys, json
+import sys
+import json,jsons
 from model.Models import *
 
 DEFAULT_PATH = ""
@@ -6,27 +7,43 @@ state = {}
 
 
 def ListAreas(args):
-    """Creates """
-    pass
+    """ """
+    for a in state.keys():
+        print(a)
 
 
 def AddArea(args):
     """Creates """
+    if not len(args)==2:
+        raise ArgumentError("Error: 2 arguments required: name and type")
+    name = args[0]
+    typestr = args[1]
+    typeint = 0
+    if typestr in ['1', 'checklist', 'cl']:
+        typeint = 1
+    if typestr in ['2', 'semanticData', 'semanticdata', 'semantic_data', 'sd']:
+        typeint = 2
+    if typestr in ['3', 'freetext', 'ft']:
+        typeint = 3
 
-    pass
+    newa = Area(name, typeint,{})
+    state[name] = newa
+    print(f"Created area {name} of type {typestr}. Executing change area subprogram.")
+    ChangeArea([name])
 
 
 def RemoveArea(args):
     if not len(args) == 1:
         raise ArgumentError("Error: Name not specified")
     name = args[0]
-    for area in state:
-        if name == area.name:
-            decision = input("Do you really want to delete the {name} area? Y/n 1/0")
-            if decision in ["1", "Y", "y", "yes"]:
-                del area
-                return True
-    raise ArgumentError("Error: Name of area not found.")
+    if name in state:
+        decision = input("Do you really want to delete the {name} area? Y/n 1/0")
+        if decision in ["1", "Y", "y", "yes"]:
+            del state[name]
+            return True
+        return False
+    else:
+        raise ArgumentError("Error: Name of area not found.")
 
 
 def ChangeArea(args):
@@ -48,13 +65,13 @@ def AddDayToArea(args):
     pass
 
 
-def SaveState(args=DEFAULT_PATH):
+def SaveState(args=tuple(DEFAULT_PATH)):
     """Puts the state of diary to JSON"""
     global state
     if not len(args) == 1:
-        raise ArgumentError("Too many parameters")
-    with open(args, "w+") as j:
-        json.dump(state, j)
+        raise ArgumentError("Too many arguments.")
+    with open(args[0], "w+") as j:
+        j.write(str(jsons.dump(state)))
 
 
 def RecoverState(args=DEFAULT_PATH):
@@ -62,21 +79,20 @@ def RecoverState(args=DEFAULT_PATH):
     global state
     if not len(args) == 1:
         raise ArgumentError("Too many parameters")
-    path = args
+    path = args[0]
     try:
         with open(path, "r") as j:
-            state = json.load(j)
+            state = jsons.load(j)
     except FileNotFoundError:
         print("Error: Invalid path to JSON.")
-    return
-    pass
 
 
+# basic commands definitions
 comms = {
     "listas": Command("listarea", "lists available areas", ListAreas),
     "adda": Command("addarea", "adds area and runs the change area subprogram", AddArea, ["name", "type"]),
     "rema": Command("removearea", "removes specified area", RemoveArea, ["name"]),
-    "changea": Command("changearea", "runs the change area subprogram", ["name"]),
+    "changea": Command("changearea", "runs the change area subprogram", ChangeArea, ["name"]),
     "dispa": Command("displayarea", "displays specified area", DisplayArea, ["name"]),
     "addall": Command("adddaytoall", "runs the add day subprogram for all areas", AddDay),
     "addspec": Command("adddaytospecific", "runs the add day subprogram for specified area", AddDay, ["name"]),
@@ -89,12 +105,13 @@ comms = {
 def mainLoop(path, commands):
     global state
     print("for the list of commands type 'help'")
-    RecoverState(path)
+    RecoverState([path])
     while True:
         command, *args = input().split()
         if command == "help":
-            for c in commands:
-                print(f"{c} args:{str(*c.args)} {c.description}")
+            for c in commands.items():
+                print(f"{c[0]} args:{str(*c[1].args)} {c[1].description}")
+
         elif command == "exit":
             print("Bye")
             sys.exit()
--- model/Models.py
+++ model/Models.py
@@ -1,26 +1,42 @@
+from dataclasses import dataclass
+from typing import ClassVar
+import datetime
 # Area > Day > Item
+
+
+@dataclass
 class Area():
-    CHECKLIST = 1
-    SEMATIC_DATA = 2
-    FREE_TEXT = 3
+   # CHECKLIST: ClassVar[int] = 1
+   # SEMATIC_DATA : ClassVar[int] = 2
+   # FREE_TEXT : ClassVar[int] = 3
+    name:str
+    type:int
+    days:dict
 
-    def __init__(self, name, days, type):
+    '''def __init__(self, name, type, days=[]):
         self.name = name
-        self.days = days
         self.type = type  # checklist,semanticData,freetext
+        self.days = days'''
 
 
-class Day():
-    def __init__(self, items, date, time):
+@dataclass
+class Day:
+    date:datetime
+    items:dict
+    '''    def __init__(self, items, date, time):
         self.date = date
         self.items = items
+    '''
 
-
+@dataclass
 class Item():
+    data:dict
+    meta:dict
+    '''
     def __init__(self, data, meta):
         self.data = data
         self.meta = meta  # isDisplayed, question
-
+    '''
 
 class Command:
     def __init__(self, name, description, function, args=[]):
